adbf8869a6468d4f8dbea404893c88d2f67431f0
Danila Kovaliou <danila20022@mail.ru>
2024-07-08 01:10:15 +0300

    Assets i dell

--- app/src/main/java/com/example/myportfolio/domain/models/Bonds.kt
+++ app/src/main/java/com/example/myportfolio/domain/models/Bonds.kt
@@ -1,5 +1,5 @@
 package com.example.myportfolio.domain.models
-
+//
 data class Bonds(val name: String, val quantity: Int, val price: Double, val issuer: String, val maturityDate: String)
 
 
--- app/src/main/java/com/example/myportfolio/domain/models/Stocks.kt
+++ app/src/main/java/com/example/myportfolio/domain/models/Stocks.kt
@@ -1,3 +1,3 @@
 package com.example.myportfolio.domain.models
-
+//
 data class Stocks(val name: String, val quantity: Int, val price: Double, val symbol: String)
