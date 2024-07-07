88b48f5b3c66cb08364722c994f2c0fe06fc0d7a
Danila Kovaliou <danila20022@mail.ru>
2024-07-07 20:02:02 +0300

    .

---
+++ app/src/main/java/com/example/myportfolio/domain/models/Assets.kt
@@ -0,0 +1,5 @@
+package com.example.myportfolio.domain.models
+
+data class Assets(val name: String, val quantity: Int, val price: Double)
+
+
---
+++ app/src/main/java/com/example/myportfolio/domain/models/Bonds.kt
@@ -0,0 +1,7 @@
+package com.example.myportfolio.domain.models
+
+data class Bonds(val name: String, val quantity: Int, val price: Double, val issuer: String, val maturityDate: String)
+
+
+
+
